dbc753609e0b86bbe441237d18fdc3286001392a
robin93 <robinsinghiitd@gmail.com>
2015-09-25 17:23:25 +0530

    added plotting and arma series generation

--- arma_series_generator.py
+++ arma_series_generator.py
@@ -1,17 +1,37 @@
 from numpy import append,array
 from numpy.random import normal
+import matplotlib.pyplot as plt
 
-sigma = 1
+sigma = 2
 n = 100
+phi = [0.8]
+theta = [-0.8]
+burnin = 500
 
 def arma_generator(sigma,n,burnin=10):
-	#l = max(len(phi),len(theta))
+	l = max(len(phi),len(theta))
 	w = normal(0,sigma,n+burnin)
-	return w
+	ARMA = array([])
+	s = 0.0
+	for i in range(n+burnin):
+		if (i<1):
+			ARMA = append(ARMA,w[i])
+		else:
+			s = 0.0
+			for j in range(len(phi)):
+				s = s + phi[j]*ARMA[i-j-1]
+			for j in range(len(theta)):
+				s = s + theta[j]*w[i-j-1]
+			ARMA = append(ARMA,s+w[i])
+	return ARMA[burnin:] 
 
-print arma_generator(sigma,n)
+x = arma_generator(sigma,n)
 
+print x
 
+plt.plot(x,'r')
+plt.plot(x,'or')
+plt.show()
 
 
 
